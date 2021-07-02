8883ed3ff4e7f4f7dd81b9514f8dfe0d5f237827
CarolineF3 <caroline.fahlgren@gmail.com>
2021-07-02 17:45:23 +0200

    first commit

---
+++ itemsInStore.js
@@ -0,0 +1,147 @@
+const itemsInStore = {
+  sucess: true,
+  items: [
+    {
+      category: "books",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image:
+        "https://res.cloudinary.com/dxewmjaue/image/upload/v1625234200/storeItems/book_j9tn7x.jpg",
+      name: "GRIMOIRE",
+      price: 100,
+      isfeatured: true,
+    },
+    {
+      category: "books",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image:
+        "https://res.cloudinary.com/dxewmjaue/image/upload/v1625234200/storeItems/books_werekq.jpg",
+      name: "LOVE SPELLS",
+      price: 100,
+      isfeatured: false,
+    },
+    {
+      category: "crystals",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image:
+        "https://res.cloudinary.com/dxewmjaue/image/upload/v1625235107/storeItems/agate_xb111l.jpg",
+      name: "AGATE",
+      price: 50,
+      isfeatured: true,
+    },
+    {
+      category: "crystals",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image:
+        "https://res.cloudinary.com/dxewmjaue/image/upload/v1625235107/storeItems/amethyst_sohhha.jpg",
+      name: "AMETHYST",
+      price: 50,
+      isfeatured: false,
+    },
+    {
+      category: "crystals",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image:
+        "https://res.cloudinary.com/dxewmjaue/image/upload/v1625235108/storeItems/rosequartz_jmf6p1.jpg",
+      name: "ROSE QUARTZ",
+      price: 50,
+      isfeatured: false,
+    },
+    {
+      category: "crystals",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image:
+        "https://res.cloudinary.com/dxewmjaue/image/upload/v1625235109/storeItems/vivianite_cpnkgo.jpg",
+      name: "VIVIANITE",
+      price: 50,
+      isfeatured: false,
+    },
+    {
+      category: "incense",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image:
+        "https://res.cloudinary.com/dxewmjaue/image/upload/v1625235107/storeItems/insence_kaxff6.jpg",
+      name: "INCENSE",
+      price: 50,
+      isfeatured: true,
+    },
+    {
+      category: "incense",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image:
+        "https://res.cloudinary.com/dxewmjaue/image/upload/v1625235108/storeItems/sage_jngdgx.jpg",
+      name: "SAGE",
+      price: 60,
+      isfeatured: false,
+    },
+    {
+      category: "jewellery",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image:
+        "https://res.cloudinary.com/dxewmjaue/image/upload/v1625235107/storeItems/goldbutterflyhoops_vexc2d.jpg",
+      name: "GOLD BUTTERFLY HOOPS",
+      price: 120,
+      isfeatured: true,
+    },
+    {
+      category: "jewellery",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image:
+        "https://res.cloudinary.com/dxewmjaue/image/upload/v1625235108/storeItems/pearlhoops_vvpzl0.jpg",
+      name: "GOLD PEARL HOOPS",
+      price: 120,
+      isfeatured: false,
+    },
+    {
+      category: "jewellery",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image:
+        "https://res.cloudinary.com/dxewmjaue/image/upload/v1625235107/storeItems/goldstarearrings_emz2rh.jpg",
+      name: "GOLD STAR EARRINGS",
+      price: 120,
+      isfeatured: false,
+    },
+    {
+      category: "jewellery",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image:
+        "https://res.cloudinary.com/dxewmjaue/image/upload/v1625235107/storeItems/goldstarhoops_j6j8tv.jpg",
+      name: "GOLD STAR HOOPS",
+      price: 120,
+      isfeatured: false,
+    },
+    {
+      category: "tarot",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image:
+        "https://res.cloudinary.com/dxewmjaue/image/upload/v1625235108/storeItems/tarotcard_mkv8hj.jpg",
+      name: "TAROT DECK",
+      price: 150,
+      isfeatured: false,
+    },
+    {
+      category: "tarot",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image:
+        "https://res.cloudinary.com/dxewmjaue/image/upload/v1625235108/storeItems/tarotdeck_nm1ymu.jpg",
+      name: "VINTAGE TAROT DECK",
+      price: 150,
+      isfeatured: false,
+    },
+  ],
+};
+
+export default itemsInStore;
---
+++ server.js
@@ -0,0 +1,223 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import listEndpoints from "express-list-endpoints";
+import crypto from "crypto";
+import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+import cloudinaryFramework from "cloudinary";
+import multer from "multer";
+import cloudinaryStorage from "multer-storage-cloudinary";
+
+import itemsInStore from "./itemsInStore";
+
+dotenv.config();
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/foretaget";
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+mongoose.Promise = Promise;
+
+const ItemSchema = new mongoose.Schema({
+  category: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  isfeatured: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+const UserSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString("hex"),
+  },
+});
+
+const CartSchema = new mongoose.Schema({
+  items: { type: Array },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+const Item = mongoose.model("Item", ItemSchema);
+
+const User = mongoose.model("User", UserSchema);
+
+const Cart = mongoose.model("Cart", CartSchema);
+
+if (process.env.RESET_DB) {
+  const seedDB = async () => {
+    await Item.deleteMany();
+
+    itemsInStore.items.forEach(async (item) => {
+      const newItem = await new Item(item);
+      await newItem.save();
+    });
+  };
+  seedDB();
+}
+
+const authenticateUser = async (req, res, next) => {
+  const accessToken = req.header("Authorization");
+
+  try {
+    const user = await User.findOne({ accessToken });
+    if (user) {
+      next();
+    } else {
+      res.status(401).json({ success: false, message: "Not authorized" });
+    }
+  } catch (error) {
+    res.status(400).json({ success: false, message: "Invalid request", error });
+  }
+};
+
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send(listEndpoints(app));
+});
+
+app.post("/signup", async (req, res) => {
+  const { firstname, lastname, email, password } = req.body;
+
+  try {
+    const salt = bcrypt.genSaltSync();
+
+    const newUser = await new User({
+      firstname,
+      lastname,
+      email,
+      password: bcrypt.hashSync(password, salt),
+    }).save();
+    const newCart = await new Cart({
+      items: [],
+      userId: newCart,
+    }).save();
+
+    res.json({
+      success: true,
+      userID: newUser._id,
+      accessToken: newUser.accessToken,
+    });
+  } catch (error) {
+    res.status(400).json({ success: false, message: "Invalid request", error });
+  }
+});
+
+app.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
+      if (bcrypt.compareSync(password, user.password)) {
+        res.json({
+          success: true,
+          userID: user._id,
+          accessToken: user.accessToken,
+        });
+      } else {
+        res.status(400).json({ success: false, message: "Wrong password" });
+      }
+    } else {
+      res.status(400).json({ success: false, message: "User not found" });
+    }
+  } catch (error) {
+    res.status(400).json({ success: false, message: "Invalid request", error });
+  }
+});
+
+app.get("/items", async (req, res) => {
+  const { category, featured } = req.query;
+  if (category) {
+    try {
+      const items = await Item.find({ category: category });
+      res.status(200).json(items);
+    } catch (error) {
+      res.status(400).json({ error: "Something went wrong", details: error });
+    }
+  } else if (featured) {
+    try {
+      const items = await Item.find({ isfeatured: featured });
+      res.status(200).json(items);
+    } catch (error) {
+      res.status(400).json({ error: "Something went wrong", details: error });
+    }
+  } else {
+    try {
+      const items = await Item.find();
+      res.status(200).json(items);
+    } catch (error) {
+      res.status(400).json({ error: "Something went wrong", details: error });
+    }
+  }
+});
+
+app.get("/items/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const item = await Item.findById(id).exec();
+    res.json(item);
+  } catch {}
+});
+
+app.get("/cart/:id", authenticateUser);
+app.get("/cart/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const cart = await Cart.findOne({ userId: id });
+    res.json(cart);
+  } catch {}
+});
+
+app.listen(port, () => {
+  // eslint-disable-next-line
+  console.log(`Server running on http://localhost:${port}`);
+});
